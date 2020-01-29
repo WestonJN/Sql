50f160babfeeb004c6fe56802c2f2f4aa050c633
WestonJN <westonnyirenda@gmail.com>
2020-01-29 16:31:29 +0200

    added sql

--- part2.sql
+++ part2.sql
@@ -3,3 +3,4 @@ SELECT "firstname" FROM "customers"
 SELECT "firstname" FROM "customers" WHERE "customerid" = 1 
 UPDATE customers SET firstname= 'Lerato' WHERE customerid = 1;
 UPDATE customers SET lastname='Mabitso' WHERE customerid = 1
+DELETE from customers WHERE customerid =2
